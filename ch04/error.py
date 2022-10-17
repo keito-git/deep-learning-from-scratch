1efd94f0f306dc2e32c70102e984dbb18f9ec510
keito-git <107419344+keito-git@users.noreply.github.com>
2022-10-17 17:28:07 +0900

    Update error.py

--- ch04/error.py
+++ ch04/error.py
@@ -1,3 +1,9 @@
+import numpy as np
+
 def cross_entropy_error(y,t):
-  delta = 1e-7
+  delta = 1e-7  #発散防止
   return -np.sum(t*np.log(y+delta))
+
+
+def sum_squared_error(y, t):
+  return 0.5 * np.sum((y-t)**2)
